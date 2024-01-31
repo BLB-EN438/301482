d9c78d85c30b9f40bdcde53eba0c83b9b2463f04
Bhargav B L <blbhargav@envistacorp.com>
2024-01-31 17:51:19 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'bhargav-dev-info',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
